1ea4f066c5060144c4050ae9e623e667ff685967
developermithu <www.mithudas77@gmail.com>
2022-06-29 05:39:29 +0600

    Category wize news and refactor news component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import News from "./components/pages/News";
 import TheFooter from "./components/TheFooter";
 import TheHeader from "./components/TheHeader";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
@@ -8,7 +9,65 @@ function App() {
       <TheHeader />
       {/* <Home/> */}
 
-      <News pageSize="18"/>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <News key="general" country="us" category="general" pageSize={18} />
+          }
+        />
+        <Route
+          path="/business"
+          element={
+            <News
+              key="business"
+              country="us"
+              category="business"
+              pageSize={18}
+            />
+          }
+        />
+        <Route
+          path="/entertainment"
+          element={
+            <News
+              key="entertainment"
+              country="us"
+              category="entertainment"
+              pageSize={18}
+            />
+          }
+        />
+        <Route
+          path="/health"
+          element={
+            <News key="health" country="us" category="health" pageSize={18} />
+          }
+        />
+        <Route
+          path="/science"
+          element={
+            <News key="science" country="us" category="science" pageSize={18} />
+          }
+        />
+        <Route
+          path="/sports"
+          element={
+            <News key="sports" country="us" category="sports" pageSize={18} />
+          }
+        />
+        <Route
+          path="/technology"
+          element={
+            <News
+              key="technology"
+              country="us"
+              category="technology"
+              pageSize={18}
+            />
+          }
+        />
+      </Routes>
 
       <TheFooter />
     </>
--- src/components/pages/News.js
+++ src/components/pages/News.js
@@ -2,8 +2,21 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 import { Button } from "@material-tailwind/react";
 import Spinner from "../Spinner";
+import PropTypes from "prop-types";
 
 export default class News extends Component {
+  static defaultProps = {
+    country: "us",
+    pageSize: "18",
+    category: "general",
+  };
+
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+  };
+
   constructor() {
     super();
     this.state = {
@@ -13,9 +26,8 @@ export default class News extends Component {
     };
   }
 
-  // It's output will be shown after render() method
-  async componentDidMount() {
-    let apiUrl = `https://newsapi.org/v2/top-headlines?country=us&apiKey=d59c6e90f9624b55941a603a87667a49&page=1&pageSize=${this.props.pageSize}`;
+  async updateNews() {
+    let apiUrl = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d59c6e90f9624b55941a603a87667a49&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
     let data = await fetch(apiUrl);
     let parsedData = await data.json();
@@ -26,45 +38,39 @@ export default class News extends Component {
     });
   }
 
+  // It's output will be shown after render() method
+  async componentDidMount() {
+    this.updateNews();
+  }
+
   handlePrevClick = async () => {
-    // console.log("Prev click");
-    let apiUrl = `https://newsapi.org/v2/top-headlines?country=us&apiKey=d59c6e90f9624b55941a603a87667a49&page=${
-      this.state.page - 1
-    }&pageSize=${this.props.pageSize}`;
-    this.setState({ loading: true }); //show loading
-    let data = await fetch(apiUrl);
-    let parsedData = await data.json();
-    this.setState({
-      page: this.state.page - 1,
-      articles: parsedData.articles,
-      loading: false,
-    });
+    this.setState({ page: this.state.page - 1 });
+    this.updateNews();
   };
 
   handleNextClick = async () => {
-    // console.log("pageNumb:" + Math.ceil(this.state.totalResults / this.props.pageSize));
-    // console.log("page:" + this.state.page + 1);
-    let pageNumber = Math.ceil(this.state.totalResults / this.props.pageSize);
-    if (!(this.state.page + 1 > pageNumber)) {
-      let apiUrl = `https://newsapi.org/v2/top-headlines?country=us&apiKey=d59c6e90f9624b55941a603a87667a49&page=${
-        this.state.page + 1
-      }&pageSize=${this.props.pageSize}`;
-      this.setState({ loading: true }); //show loading
-      let data = await fetch(apiUrl);
-      let parsedData = await data.json();
-      this.setState({
-        page: this.state.page + 1,
-        articles: parsedData.articles,
-        loading: false,
-      });
+    this.setState({ page: this.state.page + 1 });
+    this.updateNews();
+  };
+
+  capitalizeCategoryName = () => {
+    const array = this.props.category.toLowerCase().split(" ");
+    for (let i = 0; i < array.length; i++) {
+      array[i] = array[i].charAt(0).toUpperCase() + array[i].slice(1);
     }
+    return array.join(" ");
   };
 
   render() {
+    document.title = `Global News - ${this.capitalizeCategoryName()}`;
+
     const currentPageNum = this.state.page + 1;
     const totalPageNum = Math.ceil(
       this.state.totalResults / this.props.pageSize
     );
+
+    console.log("current " + currentPageNum);
+    console.log("total " + totalPageNum);
 
     return (
       <section className="text-gray-600 container">
@@ -73,23 +79,31 @@ export default class News extends Component {
           {this.state.loading && <Spinner />}
 
           <div className="flex flex-wrap -m-4">
-            {!this.state.loading && this.state.articles.map((article) => {
-              return (
-                <NewsItem
-                  title={article.title ? article.title.slice(0, 60) : ""}
-                  imageUrl={
-                    article.urlToImage
-                      ? article.urlToImage
-                      : "https://dummyimage.com/768x403/de1ddb/ffffff.jpg&text=Image+not+found"
-                  }
-                  description={
-                    article.description ? article.description.slice(0, 120) : ""
-                  }
-                  newsUrl={article.url}
-                  key={article.url}
-                />
-              );
-            })}
+            {!this.state.loading &&
+              this.state.articles &&
+              this.state.articles.map((article) => {
+                return (
+                  <NewsItem
+                    key={article.url}
+                    title={article.title ? article.title.slice(0, 60) : ""}
+                    imageUrl={
+                      article.urlToImage
+                        ? article.urlToImage
+                        : "https://dummyimage.com/768x403/de1ddb/ffffff.jpg&text=Image+not+found"
+                    }
+                    description={
+                      article.description
+                        ? article.description.slice(0, 120)
+                        : ""
+                    }
+                    newsUrl={article.url}
+                    category={this.props.category}
+                    date={article.publishedAt}
+                    author={article.author}
+                    source={article.source.name}
+                  />
+                );
+              })}
           </div>
 
           <div className="flex items-center justify-between pt-12">
